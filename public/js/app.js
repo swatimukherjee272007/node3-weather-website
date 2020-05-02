707985d8a21270e12a9b81e33aaf55e5185ca840
Swati Mukherjee <swati.mukherjee@mulesoft.com>
2020-05-01 22:56:52 -0700

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ const location = search.value
 message1.textContent = 'Loading ....'
 message2.textContent = ''
 
-fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             //console.log(data.error)
